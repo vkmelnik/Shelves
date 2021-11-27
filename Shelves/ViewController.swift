54d7732155e424806026dae74b14ce92befc2557
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2021-11-27 11:50:42 +0300

    Initial commit, storyboard deleted

---
+++ Shelves/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shelves
+//
+//  Created by Vsevolod Melnik on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
